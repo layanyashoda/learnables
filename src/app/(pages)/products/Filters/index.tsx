ab1e7d338d04afa30185fdf70579b3c1f91c1f52
Layan Yashoda <44344499+layanyashoda@users.noreply.github.com>
2024-12-07 07:15:53 +0530

    Radio Button Components

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 import { Category } from '../../../../payload/payload-types'
 import { Checkbox } from '../../../_components/Checkbox'
 import { HR } from '../../../_components/HR'
-import { RadioButton } from '../../../_components/Radio'
+// import { RadioButton } from '../../../_components/Radio'
 import { useFilter } from '../../../_providers/Filter'
 
 import classes from './index.module.scss'
