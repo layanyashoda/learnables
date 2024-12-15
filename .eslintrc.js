614b1f24f8397f098f5a27929ba4df6d1ab531e7
Layan Yashoda <44344499+layanyashoda@users.noreply.github.com>
2024-12-15 08:42:21 +0530

    Test Build

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,3 +8,22 @@ module.exports = {
     'no-console': 'off',
   }
 }
+
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+]
+
+export default eslintConfig
